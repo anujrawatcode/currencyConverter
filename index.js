b558fc52c9b8e74b27062e92498ab84e106900f1
anujrawat <anujrawatcode@gmail.com>
2020-10-29 12:24:25 +0530

    initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD"), function(error, response, body) {
+        var data = JSON.parse(body);
+        var price
+    }
+});
