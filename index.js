10f7ac30d17e29c0971e5ba4f6dfbc2cd5fbdd55
anujrawat <anujrawatcode@gmail.com>
2020-10-29 20:39:14 +0530

    finished

--- index.js
+++ index.js
@@ -4,39 +4,47 @@ const request = require("request");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
 
     var crypto = req.body.crypto;
     var fiat = req.body.fiat;
-
+   
     var options = {
-        url : "https://apiv2.bitcoinaverage.com/convert/global",
-        methord : "GET",
+        url: "https://apiv2.bitcoinaverage.com/convert/global",
+        methord: "GET",
         qs: {
             from: crypto,
             to: fiat,
             amount: amount
         }
-    };
+    }
     
-    request(options), function(error, response, body) {
+
+    request(options), function (error, response, body) {
 
         var data = JSON.parse(body);
         var price = data.last;
-        var currentData = data.display_timestamp;
 
-        res.write("<p>The current data is " + currentData+ "</p>");
-        res.write();
-        res.send("<h1>The current price of "+crypto+ " is "+price+fiat+"</h1>");
+        console.log(price);
+
+        var currentData = data.time;
+
+        res.write("<p>The current data is " + currentData + "</p>");
+
+        res.send("<h1>" + amount + crypto + " is currently" + price + fiat + "</h1>");
+
+        res.send();
     }
-});
+    });
 
-app.listen(3000, function() {
-    console.log("Sevrver started in 3000");
+
+
+app.listen(3000, function () {
+    console.log("Server started in 3000");
 });
