0cf2ee44d236d5f0131f30cae5fbdc06fd904c92
anujrawat <anujrawatcode@gmail.com>
2020-10-29 19:59:06 +0530

    request in api

--- index.js
+++ index.js
@@ -11,8 +11,32 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-    request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD"), function(error, response, body) {
+
+    var crypto = req.body.crypto;
+    var fiat = req.body.fiat;
+
+    var options = {
+        url : "https://apiv2.bitcoinaverage.com/convert/global",
+        methord : "GET",
+        qs: {
+            from: crypto,
+            to: fiat,
+            amount: amount
+        }
+    };
+    
+    request(options), function(error, response, body) {
+
         var data = JSON.parse(body);
-        var price
+        var price = data.last;
+        var currentData = data.display_timestamp;
+
+        res.write("<p>The current data is " + currentData+ "</p>");
+        res.write();
+        res.send("<h1>The current price of "+crypto+ " is "+price+fiat+"</h1>");
     }
+});
+
+app.listen(3000, function() {
+    console.log("Sevrver started in 3000");
 });
